83edc6c0bf722d76be5b86f78ee23cb57235d128
hasanaligathamaniya <hasanaligathamaniya476@gmail.com>
2021-04-20 02:22:37 +0530

    rr

---
+++ admin/include/path/notification.php
@@ -0,0 +1,35 @@
+<div class="container-fluid">
+	<div class="card">
+		<div class="card-header">Notification</div>
+		<div class="card-body">
+			<form >
+			  <div class="form-group col-sm-3">
+			    <label for="exampleFormControlSelect1">Select Department</label>
+			    <select class="form-control" id="exampleFormControlSelect1">
+			    	<optgroup label="U.G">
+			    	  <option>BCA</option>
+				      <option>BSc</option>
+				      <option>B.com</option>
+				      <option>BBA</option>
+				      <option>BA</option>
+				    <optgroup label="P.G">
+				      <option>MCA</option>
+				      <option>MSc</option>
+				      <option>M.com</option>
+				      <option>MBA</option>
+				      <option>MA</option>
+			    </select>
+			  </div>
+			  <div class="form-group col-sm-6">
+			    <label for="exampleFormControlTextarea1">Message</label>
+			    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Type some thing..."></textarea>
+			  </div>
+
+			  <div class="form-group col-sm-6">
+				<button type="submit" name="add_notice" class="btn btn-primary">Notify</button>
+			  </div>
+
+			</form>
+		</div>
+	</div>
+</div>
